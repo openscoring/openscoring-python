4e7fa33b39d891e8bf0cd14db7b39656c07fd260
Villu Ruusmann <villu.ruusmann@gmail.com>
2019-02-19 10:31:48 +0200

    Registered 'openscoring' package with PyPI

--- setup.py
+++ setup.py
@@ -9,12 +9,22 @@ setup(
 	author = "Villu Ruusmann",
 	author_email = "villu.ruusmann@gmail.com",
 	url = "https://github.com/openscoring/openscoring-python",
+	download_url = "https://github.com/openscoring/openscoring-python/archive/" + __version__ + ".tar.gz",
 	license = __license__,
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Topic :: Software Development",
+		"Topic :: Scientific/Engineering"
+	],
 	packages = [
 		"openscoring"
 	],
 	install_requires = [
 		"pandas",
-		"requests"
+		"requests>=2.10.0"
 	]
 )
